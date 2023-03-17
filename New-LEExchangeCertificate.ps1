11deec802b9e6e4db76a21f3629e48ebf65398b2
hTobi02 <67925751+hTobi02@users.noreply.github.com>
2023-03-17 19:46:13 +0100

    added all available plugins, made LE_PROD new default, added functionality for using multiple plugins

--- New-LEExchangeCertificate.ps1
+++ New-LEExchangeCertificate.ps1
@@ -15,16 +15,95 @@
 #>
 
 #edit the following variables
-#path for hoarding all certificates
-$certFilePath = "C:\cert\exchange_$(get-date -format yyyy-MM-dd).pfx"
-#your domain names
-$certNames = 'mail.example.com','autodiscover.example.com'
-#your email address
-$certMail = 'mail@example.com'
-#your exchange servers
-$exchangeServers = "exchange01.example.com","exchange02.example.com"
-#your hetzner-dns api key
-$hetznerAPI = 'xxxxxx'
+  #path for hoarding all certificates
+  $certFilePath = "C:\cert\exchange_$(get-date -format yyyy-MM-dd).pfx"
+
+  #your email address
+  $certMail = 'mail@example.com'
+
+  #your domain names
+  $certNames = 'mail.example.com','autodiscover.example.com'
+  #if you need different plugins for multiple domains
+  # $certNames = 'mail.example.com','mail.example2.com','autodiscover.example.com','autodiscover.example2.com'
+
+  #your plugin names
+  #all plugins: AcmeDns,Active24,Akamai,Aliyun,All-Inkl,Aurora,AutoDNS,Azure,Beget,BlueCat,Bunny,Cloudflare,ClouDNS,Combell,Constellix,CoreNetworks,DeSEC,DMEasy,DNSimple,DNSPod,DOcean,DomainOffensive,Domeneshop,Dreamhost,DuckDNS,Dynu,EasyDNS,Easyname,FreeDNS,Gandi,GCloud,Google,GoDaddy,Hetzner,HostingDe,HurricaneElectric,IBMSoftLayer,Infoblox,Infomaniak,IONOS,ISPConfig,LeaseWeb,Linode,Loopia,LuaDns,Manual,Namecheap,NameCom,NS1,OVH,PointDNS,Porkbun,PortsManagement,Rackspace,Regru,RFC2136,Route53,Selectel,SimpleDNSPlus,Simply,SSHProxy,TotalUptime,UKFast,Windows,Yandex,Zilore,Zonomi
+  #read more here: https://poshac.me/docs/v4/Plugins/
+  $plugins = "Hetzner,Hetzner"
+  #if you need different plugins for multiple domains
+  # $plugins = "Hetzner,Cloudflare,Hetzner,Cloudflare"
+
+  #your plugin api keys (Uncomment for your needs)
+  # $token_AcmeDns = 'xxxxxx'
+  # $token_Active24 = 'xxxxxx'
+  # $token_Akamai = 'xxxxxx'
+  # $token_Aliyun = 'xxxxxx'
+  # $token_All-Inkl = 'xxxxxx'
+  # $token_Aurora = 'xxxxxx'
+  # $token_AutoDNS = 'xxxxxx'
+  # $token_Azure = 'xxxxxx'
+  # $token_Beget = 'xxxxxx'
+  # $token_BlueCat = 'xxxxxx'
+  # $token_Bunny = 'xxxxxx'
+  # $token_Cloudflare = 'xxxxxx'
+  # $token_ClouDNS = 'xxxxxx'
+  # $token_Combell = 'xxxxxx'
+  # $token_Constellix = 'xxxxxx'
+  # $token_CoreNetworks = 'xxxxxx'
+  # $token_DeSEC = 'xxxxxx'
+  # $token_DMEasy = 'xxxxxx'
+  # $token_DNSimple = 'xxxxxx'
+  # $token_DNSPod = 'xxxxxx'
+  # $token_DOcean = 'xxxxxx'
+  # $token_DomainOffensive = 'xxxxxx'
+  # $token_Domeneshop = 'xxxxxx'
+  # $token_Dreamhost = 'xxxxxx'
+  # $token_DuckDNS = 'xxxxxx'
+  # $token_Dynu = 'xxxxxx'
+  # $token_EasyDNS = 'xxxxxx'
+  # $token_Easyname = 'xxxxxx'
+  # $token_FreeDNS = 'xxxxxx'
+  # $token_Gandi = 'xxxxxx'
+  # $token_GCloud = 'xxxxxx'
+  # $token_Google = 'xxxxxx'
+  # $token_GoDaddy = 'xxxxxx'
+  # $token_Hetzner = 'xxxxxx'
+  # $token_HostingDe = 'xxxxxx'
+  # $token_HurricaneElectric = 'xxxxxx'
+  # $token_IBMSoftLayer = 'xxxxxx'
+  # $token_Infoblox = 'xxxxxx'
+  # $token_Infomaniak = 'xxxxxx'
+  # $token_IONOS = 'xxxxxx'
+  # $token_ISPConfig = 'xxxxxx'
+  # $token_LeaseWeb = 'xxxxxx'
+  # $token_Linode = 'xxxxxx'
+  # $token_Loopia = 'xxxxxx'
+  # $token_LuaDns = 'xxxxxx'
+  # $token_Manual = 'xxxxxx'
+  # $token_Namecheap = 'xxxxxx'
+  # $token_NameCom = 'xxxxxx'
+  # $token_NS1 = 'xxxxxx'
+  # $token_OVH = 'xxxxxx'
+  # $token_PointDNS = 'xxxxxx'
+  # $token_Porkbun = 'xxxxxx'
+  # $token_PortsManagement = 'xxxxxx'
+  # $token_Rackspace = 'xxxxxx'
+  # $token_Regru = 'xxxxxx'
+  # $token_RFC2136 = 'xxxxxx'
+  # $token_Route53 = 'xxxxxx'
+  # $token_Selectel = 'xxxxxx'
+  # $token_SimpleDNSPlus = 'xxxxxx'
+  # $token_Simply = 'xxxxxx'
+  # $token_SSHProxy = 'xxxxxx'
+  # $token_TotalUptime = 'xxxxxx'
+  # $token_UKFast = 'xxxxxx'
+  # $token_Windows = 'xxxxxx'
+  # $token_Yandex = 'xxxxxx'
+  # $token_Zilore = 'xxxxxx'
+  # $token_Zonomi = 'xxxxxx'
+
+  #your exchange server/s
+  $exchangeServers = "exchange01.example.com","exchange02.example.com"
 #stop editing from here
 
 #password for privatekey
@@ -41,9 +120,13 @@ if(((Get-Module -ListAvailable | Where-Object {$_.Name -eq "PoSh-ACME"}).count) 
 }
 
 #set ACME-Server 
-#LE_STAGE = LetsEncrypt testing
-#LE_PROD = LetsEncrypt productive
-Set-PAServer LE_STAGE
+# LE_PROD = LetsEncrypt productive
+# LE_STAGE = LetsEncrypt testing
+# BUYPASS_PROD = Buypass productive
+# BUYPASS_TEST = Buypass testing
+# ZEROSSL_PROD = ZeroSSL productive
+#or type your own acme server url
+Set-PAServer LE_PROD
 
 #if first time using PoSh-ACME, create ACME-Account with mail, accept terms of use and set private key to rsa
 if(!((Get-PAAccount -List).id.count) -ge 1){
@@ -53,11 +136,27 @@ if(!((Get-PAAccount -List).id.count) -ge 1){
 #if no cert was yet requested
 if((Get-PACertificate $certNames[0]).Subject.count -eq 0){
     #set Plugin Args for HetznerDNS
-    $pArgs = @{
-        HetznerToken = ConvertTo-SecureString -AsPlainText -Force -String $hetznerAPI
+    $tokens=Get-Variable -Name "token_*"
+    foreach($token in $tokens){
+      if($null -ne $token.Value){
+        $pluginName = $($token.Name.Replace('token_',''))
+        $tokenName = $pluginName+"Token"
+        $pArgs += @{
+          "$tokenName" = ConvertTo-SecureString -AsPlainText -Force -String $($token.value)
+        }
+      } else {
+        Write-Error "No DNS-Plugin specified"
+        exit
+      }
+    }
+    if ($null -ne $tokens){
+      $command += "-Plugin $plugins -PluginArgs `$pArgs"
+    } else {
+      Write-Error "No DNS-Plugin specified"
+      exit
     }
     #request new certificate
-    New-PACertificate $certNames -Plugin Hetzner -PluginArgs $pArgs -AcceptTOS -Contact $certMail
+    New-PACertificate $certNames $command -AcceptTOS -Contact $certMail
 }else {
     #renew current certificate
     Submit-Renewal -MainDomain $certNames[0]
